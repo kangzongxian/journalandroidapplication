bee6f597be143c04b6e13b3e33ec72c3ec996bbb
kangzongxian <kangzongxian88@gmail.com>
2022-06-17 01:07:59 +0800

    UI Updates

--- app/src/main/java/com/example/self/MainActivity.java
+++ app/src/main/java/com/example/self/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button getStartedButton;
@@ -14,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the elevation of the top bar to 0
+//        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
 
         // Get the button, set it such that when click, go to Login Page
         getStartedButton = findViewById(R.id.startButton);

eb9283f1043fcb5fc0532b210b5ed237c2db440a
kangzongxian <kangzongxian88@gmail.com>
2022-06-17 01:22:46 +0800

    Application Functionalities Working

--- app/src/main/java/com/example/self/MainActivity.java
+++ app/src/main/java/com/example/self/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.self;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.Objects;
+
+import util.JournalApi;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button getStartedButton;
+
+    // Firebase Stuff
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +41,72 @@ public class MainActivity extends AppCompatActivity {
         // Set the elevation of the top bar to 0
 //        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
 
+        // If we already have a user, we should just get the user details straight away
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null) {
+                    currentUser = firebaseAuth.getCurrentUser();
+                    String currentUserId = currentUser.getUid();
+
+                    // Find all the journals related to the user
+                    collectionReference.whereEqualTo("userId", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                    if (error != null) {
+                                        return;
+                                    }
+
+                                    String name;
+                                    // Set the JournalApi to that of the user
+                                    if (!value.isEmpty()) {
+                                        for (QueryDocumentSnapshot snapshot : value) {
+                                            JournalApi journalApi = JournalApi.getInstance();
+                                            journalApi.setUserId(snapshot.getString("userId"));
+                                            journalApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(MainActivity.this,
+                                                    JournalListActivity.class));
+                                            // Finish cause we don't want them to come back
+                                            // To the MainActivity
+                                            finish();
+                                        }
+                                    }
+                                }
+                            });
+
+                } else {
+
+                }
+            }
+        };
+
         // Get the button, set it such that when click, go to Login Page
         getStartedButton = findViewById(R.id.startButton);
 
         // Go from MainActivity page to Login page
         getStartedButton.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    // Once pause, we remove the listener
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
